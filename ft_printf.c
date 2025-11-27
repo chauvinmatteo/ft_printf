b8cf624a51f10f8a5e9a3f7843c5603e55aee69e
Matteo Chauvin <mchauvin@z4r12p1.42lyon.fr>
2025-11-27 11:00:49 +0100

    Added better main for printf

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: mchauvin <mchauvin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/18 09:24:52 by mchauvin          #+#    #+#             */
-/*   Updated: 2025/11/18 17:03:15 by mchauvin         ###   ########.fr       */
+/*   Updated: 2025/11/27 10:58:50 by mchauvin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,12 @@
 
 int	ft_printf(const char *format, ...)
 {
-	int	cpr;
-	int	i;
+	int		cpr;
+	int		i;
+	va_list	args;
 
-	va_list(args);
+	if (!format)
+		return (-1);
 	va_start(args, format);
 	i = 0;
 	cpr = 0;
@@ -39,15 +41,28 @@ int	ft_printf(const char *format, ...)
 	return (cpr);
 }
 /*
+#include <stdio.h>
+
 int	main(void)
 {
-	ft_printf("%c\n");
-	ft_printf("%s\n");
-	ft_printf("%p\n");
-	ft_printf("%i\n");
-	ft_printf("%d\n");
-	ft_printf("%X\n");
-	ft_printf("%x\n");
-	ft_printf("%u\n");
+	ft_printf("%c\n", 'A');
+	printf("%c\n", 'A');
+	ft_printf("%s\n", "Test string");
+	printf("%s\n", "Test string");
+	ft_printf("%p\n", "Test adress");
+	printf("%p\n", "Test adress");
+	ft_printf("%i\n", 555);
+	printf("%i\n", 555);
+	ft_printf("%d\n", -555);
+	printf("%d\n", -555);
+	ft_printf("%X\n", 256);
+	printf("%X\n", 256);
+	ft_printf("%x\n", 256);
+	printf("%x\n", 256);
+	ft_printf("%u\n", 256);
+	printf("%u\n", 256);
 	ft_printf("%%\n");
+	printf("%%\n");
+	ft_printf("%d", ft_printf(NULL));
+	printf("%d", printf(NULL));
 }*/
