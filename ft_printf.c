0e1b8fecde09ed927578fabf3a5d9fd5cc1440e8
Matteo <229449054+chauvinmatteo@users.noreply.github.com>
2025-12-03 15:37:33 +0100

    Added security

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: mchauvin <mchauvin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/18 09:24:52 by mchauvin          #+#    #+#             */
-/*   Updated: 2025/11/28 13:59:54 by mchauvin         ###   ########.fr       */
+/*   Updated: 2025/12/03 15:36:34 by mchauvin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 int	ft_printf(const char *format, ...)
 {
-	int		cpr;
 	int		i;
+	int		cpr;
 	va_list	args;
 
 	if (!format)
@@ -25,44 +25,43 @@ int	ft_printf(const char *format, ...)
 	cpr = 0;
 	while (format[i])
 	{
+		if (format[i] == '%' && !format[i + 1])
+			return (-1);
 		if (format[i] == '%')
 		{
 			cpr += ft_dispatch_format(format[i + 1], args);
-			i += 2;
-		}
-		else
-		{
-			write(1, &format[i], 1);
-			cpr++;
 			i++;
 		}
+		else
+			cpr += write(1, &format[i], 1);
+		i++;
 	}
 	va_end(args);
 	return (cpr);
 }
-/*
-#include <stdio.h>
 
-int	main(void)
-{
-	ft_printf("%c\n", 'A');
-	printf("%c\n", 'A');
-	ft_printf("%s\n", "Test string");
-	printf("%s\n", "Test string");
-	ft_printf("%p\n", "Test adress");
-	printf("%p\n", "Test adress");
-	ft_printf("%i\n", 555);
-	printf("%i\n", 555);
-	ft_printf("%d\n", -555);
-	printf("%d\n", -555);
-	ft_printf("%X\n", 256);
-	printf("%X\n", 256);
-	ft_printf("%x\n", 256);
-	printf("%x\n", 256);
-	ft_printf("%u\n", 256);
-	printf("%u\n", 256);
-	ft_printf("%%\n");
-	printf("%%\n");
-	ft_printf("%d", ft_printf(NULL));
-	printf("%d", printf(NULL));
-}*/
+// #include <stdio.h>
+
+// int	main(void)
+// {
+// 	ft_printf("%c\n", 'A');
+// 	printf("%c\n", 'A');
+// 	ft_printf("%s\n", "Test string");
+// 	printf("%s\n", "Test string");
+// 	ft_printf("%p\n", "Test adress");
+// 	printf("%p\n", "Test adress");
+// 	ft_printf("%i\n", 555);
+// 	printf("%i\n", 555);
+// 	ft_printf("%d\n", -555);
+// 	printf("%d\n", -555);
+// 	ft_printf("%X\n", 256);
+// 	printf("%X\n", 256);
+// 	ft_printf("%x\n", 256);
+// 	printf("%x\n", 256);
+// 	ft_printf("%u\n", 256);
+// 	printf("%u\n", 256);
+// 	ft_printf("%%\n");
+// 	printf("%%\n");
+// 	ft_printf("%d", ft_printf(NULL));
+// 	printf("%d", printf(NULL));
+// }
